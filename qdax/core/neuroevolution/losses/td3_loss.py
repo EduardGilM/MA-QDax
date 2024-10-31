3ee1c6ed8853ff8c1ad3ffbaef08a9cdb0ea726b
EduardGil <eduardgilmagraner@gmail.com>
2024-10-31 17:23:34 +0100

    Revert "Imposible hacer pga ma"
    This reverts commit 15f9d5a1f1ed598b40f2810db700a6d46ac94010.

--- qdax/core/neuroevolution/losses/td3_loss.py
+++ qdax/core/neuroevolution/losses/td3_loss.py
@@ -45,7 +45,10 @@ def make_td3_loss_fn(
     ) -> jnp.ndarray:
         """Policy loss function for TD3 agent"""
         
-        action = policy_fn(policy_params, transitions.obs)
+        print("Id:", id)
+        print("Obs:", env.map_global_obs_to_agents(transitions.obs))
+        print("Transitions obs:", transitions.obs)
+        action = policy_fn(policy_params[id], env.map_global_obs_to_agents(transitions.obs)[id])
         q_value = critic_fn(
             critic_params, obs=transitions.obs, actions=action  # type: ignore
         )
